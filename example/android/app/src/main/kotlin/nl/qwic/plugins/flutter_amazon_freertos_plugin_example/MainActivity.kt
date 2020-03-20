8f2a72926007c954b19207f0ee7ddc41da0b1162
Sri Majji <srimajji@users.noreply.github.com>
2020-03-20 21:15:38 +0100

    :construction: init flutter amazon freertos plugin :construction:

---
+++ example/android/app/src/main/kotlin/nl/qwic/plugins/flutter_amazon_freertos_plugin_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package nl.qwic.plugins.flutter_amazon_freertos_plugin_example
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ ios/Classes/FlutterAmazonFreertosPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterAmazonFreertosPlugin : NSObject<FlutterPlugin>
+@end
