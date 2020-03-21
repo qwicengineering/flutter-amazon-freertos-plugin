65ebdd3a0ee51aeebb2e0791f197975940d1ee64
Sri Majji <srimajji@users.noreply.github.com>
2020-03-21 18:53:44 +0100

    ios: add ble api scaffolding
    * isAvailable
    * isOn
    * startScanning
    * stopScanning
    * connectToDevice
    * disconnectFromDevice
    * discoverServices
    * listServices
    * readCharacteristic
    * readDescriptor
    * writeDescriptor
    * writeCharacteristic
    * setNotification
    * getMtu
    * setMtu

--- ios/Classes/FreeRTOSBluetooth.swift
+++ ios/Classes/FreeRTOSBluetooth.swift
@@ -2,12 +2,69 @@ import UIKit
 import AmazonFreeRTOS
 import AWSMobileClient
 import AWSIoT
-import plugin_scaffold
+import CoreBluetooth
 
 class FreeRTOSBluetooth {
     let awsFreeRTOSManager = AmazonFreeRTOSManager.shared
+    
+    func isAvailable(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("is bluetooth available")
+    }
 
     func isOn(call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(awsFreeRTOSManager.central?.state == .powerdOn)
+        result("is bluetooth on")
     }
+    
+    func startScanning(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func stopScanning(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func connectToDevice(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func disconnectFromDevice(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func discoverServices(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+
+    func listServices(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func readCharacteristic(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func readDescriptor(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func writeDescriptor(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result (FlutterMethodNotImplemented)
+    }
+    
+    func writeCharacteristic(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result (FlutterMethodNotImplemented)
+    }
+    
+    func setNotification(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func getMtu(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
+    func setMtu(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result(FlutterMethodNotImplemented)
+    }
+    
 }
--- ios/Classes/SwiftFlutterAmazonFreertosPlugin.swift
+++ ios/Classes/SwiftFlutterAmazonFreertosPlugin.swift
@@ -1,16 +1,33 @@
 import Flutter
 import UIKit
+import plugin_scaffold
+import CoreBluetooth
 
 let pkgName = "nl.qwic.plugins.flutter_amazon_freertos_plugin"
 
 public class SwiftFlutterAmazonFreeRTOSPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: pkgName, binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterAmazonFreeRTOSPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let plugin = FreeRTOSBluetooth()
+        _ = createPluginScaffold(
+            messenger: registrar.messenger(),
+            channelName: pkgName,
+            methodMap: [
+                "isAvailable": plugin.isAvailable,
+                "isOn": plugin.isOn,
+                "startScanning": plugin.startScanning,
+                "stopScanning": plugin.stopScanning,
+                "connectToDevice": plugin.connectToDevice,
+                "disconnectFromDevice": plugin.disconnectFromDevice,
+                "discoverServices": plugin.discoverServices,
+                "listServices": plugin.listServices,
+                "readCharacteristic": plugin.readCharacteristic,
+                "readDescriptor": plugin.readDescriptor,
+                "writeDescriptor": plugin.writeDescriptor,
+                "writeCharacteristic": plugin.writeCharacteristic,
+                "setNotification": plugin.setNotification,
+                "getMtu": plugin.getMtu,
+                "setMtu": plugin.setMtu
+        ])
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
 }
