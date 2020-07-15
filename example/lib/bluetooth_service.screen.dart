737f4b0f74568e36f154df1cf30c91ed1b0aee6d
Freddy Jimenez <fjimenez@qwic.nl>
2020-07-15 12:02:26 +0200

    characteristics values read

--- example/lib/bluetooth_service.screen.dart
+++ example/lib/bluetooth_service.screen.dart
@@ -21,6 +21,14 @@ class BluetoothServiceScreen extends StatelessWidget {
         FreeRTOSDevice device = bluetoothStore.connectedDevices[service.deviceUUID];
         List characteristics = service.characteristics;
 
+        _readCharacteristic(BluetoothCharacteristic characteristic) async {
+            print("before reading: ${characteristic.value}");
+            await characteristic.readValue();
+            print("encoded: ${characteristic.value}");
+            print("decoded: ${decodeToInt(characteristic.value)}");
+            return decodeToInt(characteristic.value);
+        }
+
         return Observer(name: "BluetoothScreen",
             builder: (_) => Scaffold(
                 appBar: AppBar(
@@ -35,19 +43,29 @@ class BluetoothServiceScreen extends StatelessWidget {
                                 flex: 1,
                                 child: Container(
                                     child: ListView.builder(
-                                            itemCount: characteristics.length,
-                                            itemBuilder: (context, index) {
-                                                return Container(
-                                                        height: 100,
-                                                        child: Column(
-                                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                                            children: <Widget>[
-                                                                Text("Id: ${characteristics[index].uuid}"),
-                                                                Text("isNotifying: ${characteristics[index].isNotifying}"),
-                                                            ]
-                                                        )
-                                                    );
-                                            }
+                                        itemCount: characteristics.length,
+                                        itemBuilder: (context, index) {
+                                            return Padding(
+                                                padding: const EdgeInsets.all(16.0),
+                                                child: new FutureBuilder(
+                                                    future: _readCharacteristic(characteristics[index]),
+                                                    builder: (context, snapshot) {
+                                                        return Container(
+                                                            height: 100,
+                                                            child: Column(                                                            
+                                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                                children: <Widget>[
+                                                                    Text("Id: ${characteristics[index].uuid}"),
+                                                                    Text("isNotifying: ${characteristics[index].isNotifying}"),
+                                                                    Text("Value: ${snapshot.connectionState == ConnectionState.done ? snapshot.data : "loading"}"),
+                                                                    OutlineButton(child: Text("Log value (check logs)"), onPressed: () => _readCharacteristic(characteristics[index]))                                                                
+                                                                ]
+                                                            )
+                                                        );
+                                                    }
+                                                ),
+                                            );                                            
+                                        }
                                     )
                                 ),
                             ),
